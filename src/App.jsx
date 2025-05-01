24931f717197a08c86af8a73763c28e80117a589
kaushik <prajapatikaushik6392@gmail.com>
2025-05-01 12:39:14 +0530

    create and setup folder stucture for The-Blog-News-Hub

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import News from './Components/News'
+
+const App = () => {
+  return (
+    <div className='Container  '>
+     <div className='news-blog-pp'>
+     <News/>
+     </div>
+    </div>
+  )
+}
+
+export default App
