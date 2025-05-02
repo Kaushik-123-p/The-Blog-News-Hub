27118dfe232fa14f766ec068b6a9d4783d1105ca
kaushik <prajapatikaushik6392@gmail.com>
2025-05-02 09:26:37 +0530

    build basic stracture using tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import News from './Components/News'
 
 const App = () => {
   return (
-    <div className='Container  '>
-     <div className='news-blog-pp'>
+    <div className='Container  w-full h-screen bg-gradient-to-r from-teal-400 to-blue-500 text-white grid place-items-center'>
+     <div className='news-blog-pp w-[95vw] h-[95vmin] mx-auto bg-zinc-900 rounded-lg shadow-4xl p-4 sm:p-6 lg:p-8'>
      <News/>
      </div>
     </div>
